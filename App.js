25cde58b3bda7dc15367d880fa037fe4d9ff5513
Sean Robenalt <srob0722@gmail.com>
2024-09-03 12:21:48 -0400

    sign in with farcaster

--- App.js
+++ App.js
@@ -1,10 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "fastestsmallesttextencoderdecoder";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Linking } from "react-native";
+import { AuthKitProvider, useSignIn } from "@farcaster/auth-kit";
+import { useEffect, useCallback } from "react";
+
+const config = {
+  rpcUrl: "https://mainnet.optimism.io",
+  domain: "sean07.com",
+  siweUri: "https://sean07.com",
+  redirectUrl: "exp://10.0.0.171:8081",
+};
 
 export default function App() {
   return (
+    <AuthKitProvider config={config}>
+      <Content />
+      <StatusBar style="auto" />
+    </AuthKitProvider>
+  );
+}
+
+function Content() {
+  const {
+    signIn,
+    url,
+    isError,
+    isSuccess,
+    connect,
+    reconnect,
+    channelToken,
+    data,
+    validSignature,
+  } = useSignIn();
+
+  const onClick = useCallback(() => {
+    if (isError) {
+      reconnect();
+    }
+    signIn();
+
+    if (url) {
+      Linking.openURL(url);
+    }
+  }, [isError, reconnect, signIn, url]);
+
+  useEffect(() => {
+    if (!channelToken) {
+      connect();
+    }
+  }, [channelToken, connect]);
+
+  const authenticated = isSuccess && validSignature;
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {authenticated ? (
+        <View>
+          <Text>Signed in!</Text>
+          <Text>{data?.username}</Text>
+        </View>
+      ) : (
+        <View>
+          <Button onPress={onClick} title="Sign in">
+            Sign in
+          </Button>
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +75,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+#### Expo Sign in With Farcaster Starter
+
+Expo app implementing [Sign in With Farcaster](https://github.com/farcasterxyz/protocol/discussions/110) for quickstarting Farcaster mobile clients. Copied from [expo demo here](https://github.com/farcasterxyz/auth-monorepo/tree/main/examples/authkit-expo-demo).
